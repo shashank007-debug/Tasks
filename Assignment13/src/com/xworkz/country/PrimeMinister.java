37246a28fa04b7c82c4e58d498607066cc5887db
shashank007 <shashankmn066@gmail.com>
2025-03-24 11:18:44 +0530

    "uploading assignment"

--- Assignment13/src/com/xworkz/country/PrimeMinister.java
+++ Assignment13/src/com/xworkz/country/PrimeMinister.java
@@ -4,17 +4,23 @@ public class PrimeMinister {
     String name;
     int age;
     int termYears;
-    Minister minister1 = new Minister("Amit Shah","Home");
-    Minister minister2 = new Minister("Antony Blinken", "Foreign Affairs");
-    Minister minister3 = new Minister("Suella Braverman", "Justice");
-    Minister minister4 = new Minister("Mélanie Joly", "Health");
-    Minister minister5 = new Minister("Penny Wong", " Defense");
+
+    Minister minister1 = new Minister("Amit shah ","Education");
+    Minister minister2 = new Minister("Amit shah ","Education");
+    Minister minister3 = new Minister("Amit shah ","Education");
+    Minister minister4 = new Minister("Amit shah ","Education");
+    Minister minister5 = new Minister("Amit shah ","Education");
     Minister[] ministers = {minister1, minister2, minister3, minister4, minister5};
 
     PrimeMinister(String name, int age, int termYears){
         this.name = name;
         this.age = age;
         this.termYears = termYears;
+
+
+
+
+
     }
     void display(){
         System.out.println("The PrimeMinister name is : "+this.name);
