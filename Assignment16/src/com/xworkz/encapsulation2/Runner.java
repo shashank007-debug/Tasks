2a84cf9fd6ba795101889bdd9641cffb48fb7ee2
shashank007 <shashankmn066@gmail.com>
2025-03-28 19:55:29 +0530

    "uploading assignment"

--- Assignment16/src/com/xworkz/encapsulation2/Runner.java
+++ Assignment16/src/com/xworkz/encapsulation2/Runner.java
@@ -8,8 +8,10 @@ public class Runner {
         Showroom showroom = new Showroom();
         Manufacturer manufacturer = new Manufacturer();
         manufacturer.initCar(showroom);
+        System.out.println("================================");
 
         Customer customer = new Customer();
         customer.display(showroom);
+        System.out.println("================================");
     }
 }
