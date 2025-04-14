24d50de37c208ffc933d6bedcefce539cf7697e3
shashank007 <shashankmn066@gmail.com>
2025-04-14 17:40:07 +0530

    "uploading assignment"

---
+++ assignment20/src/com/xworkz/finalKeyword/runner/DogRunner.java
@@ -0,0 +1,12 @@
+package com.xworkz.finalKeyword.runner;
+
+import com.xworkz.finalKeyword.internal.Dog;
+
+public class DogRunner {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.bark();
+        Dog dog1 = new Dog();
+        dog1.bark();
+    }
+}
