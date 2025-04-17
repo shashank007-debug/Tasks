5634c5cc0448bf0074f7cd99944ef9dd408a4212
shashank007 <shashankmn066@gmail.com>
2025-04-17 19:43:21 +0530

    "Aptitude"

---
+++ Armstrong/src/Armstrong.java
@@ -0,0 +1,7 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int num = 253;
+        CheckArmstrong checkArmstrong = new CheckArmstrong();
+        System.out.println(checkArmstrong.isArmstrong(num));
+    }
+}
---
+++ Armstrong/src/CheckArmstrong.java
@@ -0,0 +1,16 @@
+public class CheckArmstrong {
+    public boolean isArmstrong(int num){
+        int original = num;
+        int result = 0;
+        int digit = 0;
+        while(num != 0){
+            digit = num % 10;
+            result +=( digit * digit * digit);
+            // result += (digit*10)
+            num /= 10;
+
+        }
+        return (original == result);
+
+    }
+}
